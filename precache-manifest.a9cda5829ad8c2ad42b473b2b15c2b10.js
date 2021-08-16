52184f362b363ec391e5ee52abe502fefa06262c
nafalam <alam.nafizul63@gmail.com>
2021-08-16 17:47:38 +0100

    Updates

---
+++ precache-manifest.a9cda5829ad8c2ad42b473b2b15c2b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cb26052b255b851b876",
+    "url": "/simple-react-app/static/css/main.e1945353.chunk.css"
+  },
+  {
+    "revision": "6cb26052b255b851b876",
+    "url": "/simple-react-app/static/js/main.63f03a6d.chunk.js"
+  },
+  {
+    "revision": "b3214938f708e55ad090",
+    "url": "/simple-react-app/static/js/runtime~main.d088d395.js"
+  },
+  {
+    "revision": "8542735b8d4e4ae5d8dc",
+    "url": "/simple-react-app/static/js/2.e0498460.chunk.js"
+  },
+  {
+    "revision": "248c97fab2ba040ea8c96c46aa31f216",
+    "url": "/simple-react-app/index.html"
+  }
+];
